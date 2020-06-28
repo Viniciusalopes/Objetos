0c20c55bfc7a91dac8e6e37b129c920a886dba33
Viniciusalopes <suporte@viniciusalopes.com.br>
2020-06-28 16:39:33 -0300

    Cabeçalho da licença

--- csharp/Documentos/Ctps.cs
+++ csharp/Documentos/Ctps.cs
@@ -1,8 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/// <summary>
+///     Documentos comuns a uma Pessoa Física.
+/// </summary>
+/// <remarks>
+///     Licença MIT
+///     Copyright(c) 2020 Viniciusalopes Tecnologia
+///     
+///     A permissão é concedida, gratuitamente, a qualquer pessoa que obtenha uma cópia deste software e dos 
+///     arquivos de documentação associados (o "Software"), para negociar no Software sem restrições, 
+///     incluindo, sem limitação, os direitos de uso, cópia, modificação, fusão, publicar, distribuir, 
+///     sublicenciar e/ou vender cópias do Software e permitir que as pessoas a quem o Software é fornecido 
+///     o façam, sob as seguintes condições:
+///     
+///     O aviso de direitos autorais acima e este aviso de permissão devem ser incluídos em todas as cópias 
+///     ou partes substanciais do Software.
+///     
+///     O SOFTWARE É FORNECIDO "TAL COMO ESTÁ", SEM GARANTIA DE QUALQUER TIPO, EXPRESSA OU IMPLÍCITA, 
+///     INCLUINDO MAS NÃO SE LIMITANDO A GARANTIAS DE COMERCIALIZAÇÃO, ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA 
+///     E NÃO INFRAÇÃO. EM NENHUM CASO OS AUTORES OU TITULARES DE DIREITOS AUTORAIS SERÃO RESPONSÁVEIS POR 
+///     QUALQUER REIVINDICAÇÃO, DANOS OU OUTRA RESPONSABILIDADE, SEJA EM AÇÃO DE CONTRATO, TORT OU OUTRA 
+///     FORMA, PROVENIENTE, FORA OU EM CONEXÃO COM O SOFTWARE OU O USO, OU OUTROS ACORDOS NOS PROGRAMAS.
+///     -----------------------------------------------------------------------------------------------------
+///     Criação : Vovolinux
+///     Data    : 29/06/2020
+///     Projeto : Objetos genéricos para C#.
+/// </remarks>
 
 namespace Documentos
 {
@@ -16,6 +37,7 @@ namespace Documentos
         #endregion ATRIBUTOS
 
         #region GET/SET
+
         public int NumeroCtps
 		{
 			get { return numeroCtps; }
