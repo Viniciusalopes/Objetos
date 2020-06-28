fb2927989265aac886454c1300bff2f40517d9aa
Viniciusalopes <suporte@viniciusalopes.com.br>
2020-06-28 16:33:16 -0300

    Documento CTPS.

---
+++ csharp/Documentos/Ctps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Documentos
+{
+    class Ctps
+    {
+        #region ATRIBUTOS
+        
+		private int numeroCtps;
+		private string serieCtps;
+
+        #endregion ATRIBUTOS
+
+        #region GET/SET
+        public int NumeroCtps
+		{
+			get { return numeroCtps; }
+			set { numeroCtps = value; }
+		}
+
+		public string SerieCtps
+		{
+			get { return serieCtps; }
+			set { serieCtps = value; }
+		}
+		
+		#endregion GET/SET
+	}
+}
