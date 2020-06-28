2a342e158b3e13c75cc980c2fa524e8c693c19e5
viniciusalopes <suporte@viniciusalopes.com.br>
2020-06-28 02:13:58 -0300

    Pacote constantes Java

---
+++ java/Objetos/src/constantes/EnumTipoEnderecoTelefone.java
@@ -0,0 +1,39 @@
+/**
+ * Licença MIT Copyright(c) 2020 Viniciusalopes Tecnologia
+ *
+ * A permissão é concedida, gratuitamente, a qualquer pessoa que obtenha uma
+ * cópia deste software e dos arquivos de documentação associados (o
+ * "Software"), para negociar no Software sem restrições, incluindo, sem
+ * limitação, os direitos de uso, cópia, modificação, fusão, publicar,
+ * distribuir, sublicenciar e/ou vender cópias do Software e permitir que as
+ * pessoas a quem o Software é fornecido o façam, sob as seguintes condições:
+ *
+ * O aviso de direitos autorais acima e este aviso de permissão devem ser
+ * incluídos em todas as cópias ou partes substanciais do Software.
+ *
+ * O SOFTWARE É FORNECIDO "TAL COMO ESTÁ", SEM GARANTIA DE QUALQUER TIPO,
+ * EXPRESSA OU IMPLÍCITA, INCLUINDO MAS NÃO SE LIMITANDO A GARANTIAS DE
+ * COMERCIALIZAÇÃO, ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA E NÃO INFRAÇÃO. EM
+ * NENHUM CASO OS AUTORES OU TITULARES DE DIREITOS AUTORAIS SERÃO RESPONSÁVEIS
+ * POR QUALQUER REIVINDICAÇÃO, DANOS OU OUTRA RESPONSABILIDADE, SEJA EM AÇÃO DE
+ * CONTRATO, TORT OU OUTRA FORMA, PROVENIENTE, FORA OU EM CONEXÃO COM O SOFTWARE
+ * OU O USO, OU OUTROS ACORDOS NOS PROGRAMAS.
+ * -----------------------------------------------------------------------------
+ */
+package constantes;
+
+/**
+ * Tipos de Endereços e Telefones.
+ *
+ * @criacao: Vovolinux
+ * @data . : 28/06/2020
+ * @projeto: Objetos genéricos para Java.
+ */
+public enum EnumTipoEnderecoTelefone {
+    Todos,
+    Residencial,
+    Comercial,
+    Pessoal,
+    Cobrança,
+    Recados
+}
---
+++ java/Objetos/src/constantes/EnumVinculoPessoa.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package constantes;
+
+/**
+ * Vínculos de uma pessoa com outra, ou do utilizador do software com uma pessoa
+ * cadastrada.
+ *
+ * @criacao: Vovolinux
+ * @data . : 28/06/2020
+ * @projeto: Objetos genéricos para Java.
+ */
+public enum EnumVinculoPessoa {
+    Todos,
+    Cliente,
+    Fornecedor,
+    Colaborador
+}
