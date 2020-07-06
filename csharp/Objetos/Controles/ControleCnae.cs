25d99c389d26019cf666deedd45ea4821420d6b4
Viniciusalopes <suporte@viniciusalopes.com.br>
2020-07-05 21:31:36 -0300

    ControleCnae sem implementação.

---
+++ csharp/Objetos/Controles/ControleCnae.cs
@@ -0,0 +1,70 @@
+﻿
+
+using Objetos.Interfaces;
+using Objetos.Modelos.Documentos;
+using System.Collections.Generic;
+/// <licenca>
+///     Licença MIT
+///     Copyright(c) 2020 Viniciusalopes Tecnologia
+///     
+///     A permissão é concedida, gratuitamente, a qualquer pessoa que obtenha uma cópia deste software e dos 
+///     arquivos de documentação associados (o "Software"), para negociar no Software sem restrições, 
+///     incluindo, sem limitação, os direitos de uso, cópia, modificação, fusão, publicar, distribuir, 
+///     sublicenciar e/ou vender cópias do Software e permitir que as pessoas a quem o Software é fornecido 
+///     o façam, sob as seguintes condições:
+///     
+///     O aviso de direitos autorais acima e este aviso de permissão devem ser incluídos em todas as cópias 
+///     ou partes substanciais do Software.
+///     
+///     O SOFTWARE É FORNECIDO "TAL COMO ESTÁ", SEM GARANTIA DE QUALQUER TIPO, EXPRESSA OU IMPLÍCITA, 
+///     INCLUINDO MAS NÃO SE LIMITANDO A GARANTIAS DE COMERCIALIZAÇÃO, ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA 
+///     E NÃO INFRAÇÃO. EM NENHUM CASO OS AUTORES OU TITULARES DE DIREITOS AUTORAIS SERÃO RESPONSÁVEIS POR 
+///     QUALQUER REIVINDICAÇÃO, DANOS OU OUTRA RESPONSABILIDADE, SEJA EM AÇÃO DE CONTRATO, TORT OU OUTRA 
+///     FORMA, PROVENIENTE, FORA OU EM CONEXÃO COM O SOFTWARE OU O USO, OU OUTROS ACORDOS NOS PROGRAMAS.
+/// </licenca>
+/// <summary>
+///     
+///     Criação : Vovolinux
+///     Data    : 05/07/2020
+///     Projeto : Objetos genéricos para C#.
+/// </summary>
+namespace Objetos.Controles
+{
+    public class ControleCnae : ICRUD<Cnae>
+    {
+        public void Atualizar(Cnae objeto)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Cnae Buscar(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Cnae> Consultar()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Cnae> Consultar(object objeto)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Excluir(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Incluir(Cnae objeto)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Cnae ToObject(string texto)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
